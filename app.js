45d1553eb8d1501cf08457f83ef3624af181cf53
imp-sike <93368594+imp-sike@users.noreply.github.com>
2022-05-05 22:56:10 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 3000 });
+
+
+console.log("Started WebSocket server")
+wss.on('connection', function connection(ws) {
+  ws.onmessage = (message) => {
+    updateClients(message.data);
+  };
+});
+
+const updateClients = (message) => {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+};
