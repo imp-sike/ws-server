cd0fdff7018864484fe84d1d8d72c407e0776d51
Sulav Parajuli <impsike@pop-os.localdomain>
2022-05-05 23:23:30 +0545

    Newest

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const WebSocket = require('ws');
+const { Server } = require('ws');
+const express = require('express')
 
-const wss = new WebSocket.Server({ port: 3000 });
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+  const wss = new Server({ server });
 
 
 console.log("Started WebSocket server")
